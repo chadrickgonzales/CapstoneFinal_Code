2f73450e98ae3b3e66540839558d0a4a1f08d2ec
SenseiRands <senseirnd04@gmail.com>
2024-11-04 15:37:08 +0800

    Update other_profile_bottom_sheet.dart API key(s)

--- myapp/lib/pages/other_profile_bottom_sheet.dart
+++ myapp/lib/pages/other_profile_bottom_sheet.dart
@@ -1177,7 +1177,7 @@ Widget _buildLibraryContent(
                                   polylines: polylines,
                                   routePoints: [],
                                   apiKey:
-                                      'AIzaSyBhqi3rnTiFS6D32BIG6O6yE1kfNaLlNfQ',
+                                      'AIzaSyAocNg3WkX5ppmhc-vTf1IHvG75EM1Rr5k',
                                 );
 
                                 // Create the bottom sheet state variable
@@ -3166,7 +3166,7 @@ Widget _buildAddToListContent(
   final TextEditingController _searchController = TextEditingController();
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
   final String _apiKey =
-      'AIzaSyBhqi3rnTiFS6D32BIG6O6yE1kfNaLlNfQ'; // Replace with your actual API key
+      'AIzaSyAocNg3WkX5ppmhc-vTf1IHvG75EM1Rr5k'; // Replace with your actual API key
 
   final String _currentListId = listId;
 
@@ -3190,7 +3190,7 @@ Widget _buildAddToListContent(
 
       // Step 2: If not found in Firestore, fetch from Google Maps API
       const googleApiKey =
-          'AIzaSyBhqi3rnTiFS6D32BIG6O6yE1kfNaLlNfQ'; // Replace with your API key
+          'AIzaSyAocNg3WkX5ppmhc-vTf1IHvG75EM1Rr5k'; // Replace with your API key
       final url =
           'https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=$placeName&inputtype=textquery&fields=geometry&key=$googleApiKey';
 
